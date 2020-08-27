a612b2260f37f12ebe42c493ba0e67c9fb041329
Kiril Dolashkov <kiril.dolashkov@gmail.com>
2020-08-27 09:24:17 +0300

    Init version

---
+++ Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+open System.Net
+
+[<EntryPoint>]
+let main argv =
+
+    let method = Environment.GetEnvironmentVariable("LHP_METHOD")
+    let port = Environment.GetEnvironmentVariable("LHP_PORT")
+    let path = Environment.GetEnvironmentVariable("LHP_PATH")
+
+    let req = HttpWebRequest.Create(Uri(sprintf "http://localhost:%s/%s" port path))
+    req.Method <- method
+    use resp = req.GetResponse()
+    0
