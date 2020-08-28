1bfe37173d2257e1b2d281c0ebf17b7d415ed38e
Kiril Dolashkov <kiril.dolashkov@gmail.com>
2020-08-28 12:00:15 +0300

    Add body

--- Program.fs
+++ Program.fs
@@ -7,8 +7,16 @@ let main argv =
     let method = Environment.GetEnvironmentVariable("LHP_METHOD")
     let port = Environment.GetEnvironmentVariable("LHP_PORT")
     let path = Environment.GetEnvironmentVariable("LHP_PATH")
+    let body = Environment.GetEnvironmentVariable("LHP_BODY")
+
+    let postBytes = body |> System.Text.Encoding.ASCII.GetBytes
 
     let req = HttpWebRequest.Create(Uri(sprintf "http://localhost:%s/%s" port path))
     req.Method <- method
+    req.ContentLength <- postBytes.LongLength
+    req.ContentType <- "application/json"
+    use reqStr = req.GetRequestStream()
+    reqStr.Write(postBytes, 0, postBytes.Length)
+
     use resp = req.GetResponse()
     0
