ffcc14851cfb0fc89762f5e465b0e23050bbb7eb
Kiril Dolashkov <kiril.dolashkov@gmail.com>
2020-08-31 11:45:51 +0300

    Add deploy script

---
+++ deploy.bash
@@ -0,0 +1,4 @@
+cp /home/jedinja/deploy/localhostping/bin/Debug/netcoreapp3.1/localhostping /home/jedinja/.localhostping
+cp /home/jedinja/deploy/localhostping/bin/Debug/netcoreapp3.1/localhostping.dll /home/jedinja/.localhostping
+cp /home/jedinja/deploy/localhostping/bin/Debug/netcoreapp3.1/localhostping.runtimeconfig.json /home/jedinja/.localhostping
+cp /home/jedinja/deploy/localhostping/bin/Debug/netcoreapp3.1/FSharp.Core.dll /home/jedinja/.localhostping
