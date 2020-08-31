ed9fc6b70a3943fd8d7f7860b76ac968f915e4c3
Kiril Dolashkov <kiril.dolashkov@gmail.com>
2020-08-31 11:41:15 +0300

    Add certificate

--- Program.fs
+++ Program.fs
@@ -1,21 +1,32 @@
 ﻿open System
+open System.IO
 open System.Net
+open System.Security.Cryptography.X509Certificates
 
 [<EntryPoint>]
 let main argv =
 
-    let method = Environment.GetEnvironmentVariable("LHP_METHOD")
-    let port = Environment.GetEnvironmentVariable("LHP_PORT")
-    let path = Environment.GetEnvironmentVariable("LHP_PATH")
-    let body = Environment.GetEnvironmentVariable("LHP_BODY")
+    let method = Environment.GetEnvironmentVariable "LHP_METHOD"
+    let port = Environment.GetEnvironmentVariable "LHP_PORT"
+    let path = Environment.GetEnvironmentVariable "LHP_PATH"
+    let body = Environment.GetEnvironmentVariable "LHP_BODY"
+    let certPath = Environment.GetEnvironmentVariable "LHP_CERT"
     let postBytes = body |> System.Text.Encoding.ASCII.GetBytes
     
-    printf "ENV: %s %s %s %s" method port path body
+    printf "ENV: %s %s %s %s %s" method port path body certPath
     
-    let req = HttpWebRequest.Create(Uri(sprintf "https://localhost:%s/%s" port path))
+    let req = HttpWebRequest.Create(Uri(sprintf "https://localhost:%s/%s" port path)) :?> HttpWebRequest
     req.Method <- method
     req.ContentLength <- postBytes.LongLength
     req.ContentType <- "application/json"
+
+    match certPath with
+    | null | "" -> ()
+    | fileName ->
+        new X509Certificate2(File.ReadAllBytes fileName)
+        |> req.ClientCertificates.Add
+        |> ignore
+
     use reqStr = req.GetRequestStream()
     reqStr.Write(postBytes, 0, postBytes.Length)
 
